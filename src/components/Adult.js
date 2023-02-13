0f15124b81b54454f240f19b2e5851fc42940e08
yesaroun <akor1@naver.com>
2023-02-14 03:19:20 +0900

    fix: 프로그래밍 적합도 component 수정

---
+++ src/components/Adult.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Adult = () => {
+  return (
+    <div>
+      <Link to="https://lifehacking.co.kr">라이프해킹스쿨</Link>
+      <Link to="https://ozcodingschool.com">오크코딩스쿨</Link>
+    </div>
+  );
+};
+
+export default Adult;
