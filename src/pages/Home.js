b7be1706a50dc350ab7c467f7b600aada3c1e281
misung Kim <mizzkkim7@gmail.com>
2023-02-16 17:04:15 +0900

    feat:count

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,25 @@ import '../scss/Home.scss';
 import mainImg from '../assets/main_img.png';
 import buttonImg from '../assets/main_button.png';
 import { NavLink } from 'react-router-dom';
+import { dbFirestore } from '../fbase';
+
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 function Home() {
-  // const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+  const getData = async () => {
+    const docRef = doc(dbFirestore, 'count', 'counts');
+    const docSnap = await getDoc(docRef);
+    const count = docSnap.data().users;
+    setTotal(count);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const user_count = () => {
+    setDoc(doc(dbFirestore, 'count', 'counts'), { users: total + 1 });
+  };
 
   return (
     <div className="container">
@@ -13,7 +29,7 @@ function Home() {
         <div className="main-img">
           <img src={mainImg} alt="mainImage" />
         </div>
-        {/* <div className="count">현재까지 참여한 인원 {total}명</div> */}
+        <div className="count">현재까지 참여한 인원 {total}명</div>
         <div className="team-info">
           <NavLink to={'/team'}>테스트를 만든 사람들</NavLink>
         </div>
@@ -22,7 +38,7 @@ function Home() {
             <img
               src={buttonImg}
               alt="buttonImage"
-              // onClick={() => user_count()}
+              onClick={() => user_count()}
             />
           </NavLink>
         </div>
